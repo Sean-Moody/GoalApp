3e84c2c529da45279426de9e84f0cd6468c8faec
harjitsingh3 <singh.harjit09@gmail.com>
2023-04-19 16:38:44 -0400

    Create User model

--- Goal-App/app/models/user.rb
+++ Goal-App/app/models/user.rb
@@ -1,2 +1,54 @@
 class User < ApplicationRecord
+    #before_validation :ensure_session_token
+    after_initialize :ensure_session_token
+    
+    validates :username, :password_digest, :session_token, presence: true 
+    validates :username, :session_token, uniqueness: true 
+    validates :password, length: { minimum: 6 }, allow_nil: true 
+
+    has_many :goals
+
+    attr_reader :password 
+
+    def password=(password)
+        @password = password 
+        self.password_digest = BCrypt::Password.create(password)
+    end 
+
+    def is_same_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+
+        if user && user.is_same_password?(password)
+            user 
+        else 
+            nil 
+        end
+    end 
+
+    def ensure_session_token
+      self.session_token||= generate_unique_session_token  
+    end
+
+    def reset_session_token
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token 
+    end
+
+    def generate_unique_session_token
+        session_token = SecureRandom::urlsafe_base64
+
+        while User.exists?(session_token: session_token)
+            session_token = SecureRandom::urlsafe_base64
+        end 
+        session_token 
+    end 
+
+
+
 end
--- Goal-App/spec/models/user_spec.rb
+++ Goal-App/spec/models/user_spec.rb
@@ -2,12 +2,22 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   
+  let(:incomplete_user) { User.new(username: '', password: 'password')}
+
+  describe 'uniqueness' do
+    before :each do 
+        create(:user)
+    end
+  end 
+  
   it {should validate_presence_of(:username)}
   it {should validate_presence_of(:password_digest)}
   it {should validate_presence_of(:session_token)}
   it {should validate_length_of(:password).is_at_least(6)}
 
   it {should have_many(:goals)}
-  
+
   it {should validate_uniqueness_of(:username)}
+
+
 end
