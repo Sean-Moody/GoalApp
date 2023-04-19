05bfabc030448ff305a803f4110824935ae9f2ac
harjitsingh3 <singh.harjit09@gmail.com>
2023-04-19 17:33:24 -0400

    Create Application Controller

--- Goal-App/spec/models/user_spec.rb
+++ Goal-App/spec/models/user_spec.rb
@@ -9,15 +9,16 @@ RSpec.describe User, type: :model do
         create(:user)
     end
   end 
-  
+
   it {should validate_presence_of(:username)}
   it {should validate_presence_of(:password_digest)}
   it {should validate_presence_of(:session_token)}
   it {should validate_length_of(:password).is_at_least(6)}
+  it {should validate_uniqueness_of(:username)}
 
   it {should have_many(:goals)}
 
-  it {should validate_uniqueness_of(:username)}
+  
 
 
 end
